75c19dd45701abb2f8495456c5594d6ba8d4c9ef
cripps <rob.cripps@gmail.com>
2015-04-07 15:33:35 -0400

    Finally figured it all out! YAY

--- CRM.rb
+++ CRM.rb
@@ -1,54 +1,5 @@
-# require_relative "contact.rb"
-# require_relative "rolodex.rb"
-
-class Contact
-  attr_accessor :id, :first_name, :last_name, :email, :note
-
-  def initialize(first_name, last_name, email, note)
-    @first_name = first_name
-    @last_name = last_name
-    @email = email
-    @note = note
-  end
-
-  def to_s
-    print "First Name: #{@first_name}"
-    print "Last Name: #{@last_name}"
-    print "Email: #{@email}"
-    print "Note: #{@note}"
-  end
-end
-
-class Rolodex
-  def initialize
-    @contacts = []
-    @id = 1
-  end
-
-  def add_contact(contact)
-    contact.id = @id
-    @contacts << contact
-    @id += 1
-  end
-
-  def display_contact(contact_id)
-    @contacts.find do |contact|
-      contact.id == contact_id
-    end
-  end
-
-  def modify_contact
-  end
-
-  def display_all_contacts
-  end
-
-  def delete_contact_
-  end
-
-  def display_atrribute
-  end
-end
+require_relative "rolodex.rb"
+require_relative "contact.rb"
 
 class CRM
   def self.run
@@ -69,9 +20,10 @@ class CRM
     puts "[3] Delete a contact"
     puts "[4] Display a contact"
     puts "[5] Display an attribute"
-    puts "[6] Exit"
+    puts "[6] Display all contacts"
+    puts "[7] Exit"
     puts "--------------------------------"
-    puts "Please enter a selection (1-6)"
+    puts "Please enter a selection (1-7)"
   end
 
   def call_selection(selection)
@@ -80,7 +32,8 @@ class CRM
     delete_contact if selection == 3
     display_contact if selection == 4
     display_by_attribute if selection == 5
-    exit if selection == 6
+    display_all if selection == 6
+    exit if selection == 7
   end
 
   def main_menu
@@ -112,7 +65,7 @@ class CRM
   end
 
   def modify_contact
-  print "Enter id of user you would like to modify:"
+    print "Enter id of user you would like to modify:"
     contact_id = gets.chomp.to_i
     contact = @rolodex.find(contact_id)
     puts "Please verify if you would like the contact for #{contact} by typing 'yes' or 'no'."
@@ -126,8 +79,68 @@ class CRM
       contact.email = gets.chomp
       print "Note:"
       contact.note = gets.chomp
+      print "CONTACT MODIFIED SUCCESSFULLY"
     end
   end
+
+  def delete_contact
+    print "Enter id of user you would like to delete:"
+    contact_id = gets.chomp.to_i
+    contact = @rolodex.find(contact_id)
+    puts "Please verify if you would like to delete the contact information for #{contact} by typing 'yes' or 'no'."
+    delete_response = gets.chomp.downcase
+    if delete_response == "yes"
+      @rolodex.delete(contact_id)
+    end
+  end
+
+  def display_all
+    puts @rolodex.contacts
+  end
+
+  def display_by_attribute
+    puts "-------------------------------"
+    puts "~~~~~~~~~~ATTRIBUTES~~~~~~~~~~~"
+    puts "-------------------------------"
+    puts "[1] First Name"
+    puts "[2] Last Name"
+    puts "[3] Email"
+    puts "[4] Notes"
+    puts "Please enter the a selection (1-4)"
+
+    attribute_id = gets.chomp.to_i
+
+    option_map = {
+      1 => "first_name",
+      2 => "last_name",
+      3 => "email",
+      4 => "note",
+    }
+
+    if attribute_name = option_map[attribute_id]
+      puts @rolodex.display_attribute(attribute_name)
+    else
+      puts "Invalid option"
+    end
+    # if attribute_id == 1
+    #   @contacts.each do |contact|
+    #   print contact[0]
+    #   end
+    # elsif attribute_id == 2
+    #   @contacts.each do |contact|
+    #   print contact[1]
+    #   end
+    # elsif attribute_id == 3
+    #   @contacts.each do |contact|
+    #   print contact[2]
+    #   end
+    # elsif attribute_id == 4
+    #   @contacts.each do |contact|
+    #   print contact[3]
+    #   end
+  end
+
+
 end
 
 CRM.run
--- rolodex.rb
+++ rolodex.rb
@@ -1,1 +1,37 @@
-rolodex.rb
+class Rolodex
+  def initialize
+    @contacts = []
+    @id = 1
+  end
+
+  def add_contact(contact)
+    contact.id = @id
+    @contacts << contact
+    @id += 1
+  end
+
+  def find(contact_id)
+    @contacts.find do |contact|
+      contact.id == contact_id
+    end
+  end
+
+  def modify_contact
+  end
+
+  def contacts
+    @contacts
+  end
+
+  def delete(contact_id)
+    @contacts.delete_if do |contact|
+      contact.id == contact_id
+    end
+  end
+
+  def display_attribute(attribute_name)
+    @contacts.map do |contact|
+      contact.send(attribute_name)
+    end
+  end
+end
